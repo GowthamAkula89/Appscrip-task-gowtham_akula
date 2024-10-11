0b6cc844c0dd7dff62994b1fcaf38f9a7911c2a5
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-10-12 00:18:43 +0530

    update styles

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import GreyStrip from "@/components/GreyStrip/greyStrip";
 import Header from "@/components/Header/header";
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
@@ -9,10 +10,9 @@ export default function Home() {
       <main className={styles.main}>
         <GreyStrip/>
         <Header/>
+        <Hero/>
       </main>
-      <footer className={styles.footer}>
-        
-      </footer>
+      
     </div>
   );
 }
